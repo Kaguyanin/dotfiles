7ed7b7088b39449dca976c749ed7952c8d09f059
Guilherme Paiva <guilherme.paiva@outlook.com.br>
2019-11-05 17:57:34 -0300

    Arquivo de instalação - corrigido

---
+++ install.sh
@@ -0,0 +1,23 @@
+'## INSTALAÇÕES ##
+
+# Atualiza sistema
+sudo pacman -Syu --noconfirm
+
+# Fontes
+sudo pacman -S adobe-source-han-sans-otc-fonts dina-font gsfonts otf-latinmodern-math terminus-font ttf-dejavu ttf-liberation xorg-fonts-75dpi xorg-fonts-alias xorg-fonts-cyrillic xorg-fonts-encodings xorg-fonts-misc xorg-fonts-type1 xorg-font-util ttf-font-awesome ttf-fira-code noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk texlive-fontsextra --noconfirm
+yay -S ttf-oxygen ttf-ms-fonts siji-git ttf-symbola freetype2-infinality --noconfirm
+git clone https://github.com/powerline/fonts.git ~/powerlinefont ; ~/powerlinefont/install.sh ; rm -rf ~/powerlinefont/ 
+git clone https://github.com/terroo/fonts.git ~/fonts ; cp ~/fonts/fonts/*.otf $HOME/.local/share/fonts/ ; cp ~/fonts/fonts/*.ttf $HOME/.local/share/fonts/ ; rm -rf ~/fonts
+fc-cache -fv
+
+# Instala aplicações e denpendencias disponiveis no repositório da comunidade
+sudo pacman -S git yay i3-gaps i3blocks i3-help i3-scripts i3-scrot i3lock-color i3status-manjaro manjaro-i3-settings --noconfirm
+yay -S polybar'
+
+## CONFIGURAÇÕES ##
+
+# Configurações do i3
+cp -r ~/dotfiles/i3/* ~/.config/i3/
+
+# Configurações do Polybar
+mkdir -p ~/.config/polybar ; ins2tall -Dm644 /usr/share/doc/polybar/config $HOME/.config/polybar/config ; echo "polybar -r kobra &" > ~/.xinitrc ; rm ~/.config/polybar/config ; cp ~/dotfiles/config ~/.config/polybar/config ; polybar -r kobra
